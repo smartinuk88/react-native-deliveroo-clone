b90de1cb64aac76d1f1464bb76f6e1a3e653499b
Scott Martin <scott.martin88@live.com>
2022-11-04 13:24:11 +0100

    Env variables

--- sanity.js
+++ sanity.js
@@ -1,9 +1,10 @@
 import SanityClient from "@sanity/client";
 import ImageUrlBuilder from "@sanity/image-url";
+import { REACT_APP_SANITY_PROJECTID, REACT_APP_SANITY_DATASET } from "@env";
 
 const client = SanityClient({
-  projectId: "qwoxusny",
-  dataset: "production",
+  projectId: REACT_APP_SANITY_PROJECTID,
+  dataset: REACT_APP_SANITY_DATASET,
   useCdn: true,
   apiVersion: "2021-10-21",
 });
